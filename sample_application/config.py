f649c8ed73408d61881f966769d10b0d827d1a72
Roman Chyla <roman.chyla@gmail.com>
2018-12-12 15:46:26 -0500

    Updated alembic

--- sample_application/config.py
+++
@@ -1,6 +0,0 @@
-
-SQLALCHEMY_DATABASE_URI = 'sqlite://'
-
-# These values are necessary only if the app needs to be a client of the API
-API_TOKEN = 'this is a secret api token!'
-SAMPLE_URL = 'https://api.adsabs.harvard.edu'
